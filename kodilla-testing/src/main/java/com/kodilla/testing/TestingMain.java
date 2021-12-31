8600a77c5b8b82df8a18474fba9c1915ea71c1c4
Katarzyna Kropiak <k.kropiak@gmail.com>
2021-12-31 19:26:54 +0100

    Calculator Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        int a = -1;
+        int b = 2;
+
+        if (a + b == Calculator.add(a, b)) {
+            System.out.println("Test \"Calculator - add method\" is OK!");
+        } else {
+            System.out.println("Error!");
+        }
+        if (a - b == Calculator.subtract(a, b)) {
+            System.out.println("Test \"Calculator - subtract method\" is OK!");
+        } else {
+            System.out.println("Error!");
+        }
+
     }
+
 }
