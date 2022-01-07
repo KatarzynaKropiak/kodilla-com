47a470377e5a114f52cc848386fc6e2f3c683df9
Katarzyna Kropiak <k.kropiak@gmail.com>
2022-01-07 20:42:40 +0100

    Library Database + Tests

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/Circle.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/Circle.java
@@ -10,4 +10,5 @@ public class Circle implements Shape{
 
     public Circle() {
     }
+
 }
--- kodilla-testing/src/main/java/com/kodilla/testing/shape/Square.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/Square.java
@@ -3,6 +3,7 @@ package com.kodilla.testing.shape;
 public class Square implements Shape{
     public String getShapeName() {
         return "Square";
+
     }
     public double getArea() {
         return 12;
