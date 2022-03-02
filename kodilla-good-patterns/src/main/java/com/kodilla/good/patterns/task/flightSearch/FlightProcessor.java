1077efeb38a0671c63a95515a2e4639979a72dc9
Katarzyna Kropiak <k.kropiak@gmail.com>
2022-03-02 21:38:32 +0100

    13.05/02

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/task/flightSearch/FlightProcessor.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/task/flightSearch/FlightProcessor.java
@@ -1,40 +1,45 @@
 package com.kodilla.good.patterns.task.flightSearch;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.toList;
+public class FlightProcessor {
 
-public class Application {
-    public static void main(String[] args) {
-
+    public String getFlightsFrom(String departureAirport) {
         Flights flights = new Flights();
         String flightsFrom =
                 flights
                         .getFlights()
                         .stream()
-                        .filter(f -> f.getDeparture().equals("Warszawa"))
+                        .filter(f -> f.getDeparture().equals(departureAirport))
                         .map(Object::toString)
-                        .collect(Collectors.joining(" | ", " Flights from Warszawa: ", "."));
+                        .collect(Collectors.joining(" | ", " Flights from " + departureAirport + ": ", "."));
         System.out.println(flightsFrom);
 
+        return flightsFrom;
+    }
+
+    public String getFlightsTo(String arrivalAirport) {
+        Flights flights = new Flights();
         String flightsTo =
                 flights
                         .getFlights()
                         .stream()
-                        .filter(f -> f.getArrival().equals("Warszawa"))
+                        .filter(f -> f.getArrival().equals(arrivalAirport))
                         .map(Object::toString)
-                        .collect(Collectors.joining(" | ", " Flights to  Warszawa: ", "."));
+                        .collect(Collectors.joining(" | ", " Flights to  Warszawa " + arrivalAirport + "; ", "."));
         System.out.println(flightsTo);
 
+        return flightsTo;
+    }
 
+    public void getFlightsThrough(String transferAirport) {
+        Flights flights = new Flights();
         List<Flight> flightsFromList =
                 flights
                         .getFlights()
                         .stream()
-                        .filter(f -> f.getDeparture().equals("Warszawa"))
+                        .filter(f -> f.getDeparture().equals(transferAirport))
                         .collect(Collectors.toList());
 
 
@@ -42,17 +47,16 @@ public class Application {
                 flights
                         .getFlights()
                         .stream()
-                        .filter(f -> f.getArrival().equals("Warszawa"))
+                        .filter(f -> f.getArrival().equals(transferAirport))
                         .collect(Collectors.toList());
 
-        for (int i = 0; i<flightsToList.size(); i++) {
-            for (int j = 0; j<flightsFromList.size(); j++) {
+        for (int i = 0; i < flightsToList.size(); i++) {
+            for (int j = 0; j < flightsFromList.size(); j++) {
                 if (flightsToList.get(i).getArrival().equals(flightsFromList.get(j).getDeparture()) &
                         !flightsToList.get(i).getDeparture().equals(flightsFromList.get(j).getArrival())) {
-                    System.out.println(flightsToList.get(i).toString() + flightsFromList.get(j));
+                    System.out.println("Flights through " + transferAirport + ": " + flightsToList.get(i).toString() + flightsFromList.get(j));
+                }
             }
         }
-    }
-
     }
 }
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/task/flightSearch/Flights.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/task/flightSearch/Flights.java
@@ -2,6 +2,7 @@ package com.kodilla.good.patterns.task.flightSearch;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Flights {
 
@@ -16,6 +17,4 @@ public class Flights {
 
         return flights;
     }
-
-
 }
