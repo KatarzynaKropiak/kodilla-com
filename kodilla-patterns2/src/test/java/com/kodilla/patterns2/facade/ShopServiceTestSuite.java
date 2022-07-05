f7cc2fc7ce6a6bc270786c9fb8ec1dd4ab0945ad
Katarzyna Kropiak <k.kropiak@gmail.com>
2022-07-05 23:50:08 +0200

    24.3 WatcherFacade

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/facade/ShopServiceTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/facade/ShopServiceTestSuite.java
@@ -72,7 +72,7 @@ class ShopServiceTestSuite {
         order.addItem(new ItemDto(25L, 1));
         order.addItem(new ItemDto(11L, 3));
         try {
-            orderFacade.processOrder(order, 1L);
+            orderFacade.processOrder(order, 100L);
         } catch (OrderProcessingException e) {
             // business exception - should be handled in real application
         }
