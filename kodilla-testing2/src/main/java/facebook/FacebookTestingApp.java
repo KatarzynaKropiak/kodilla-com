c8251adf4b571cba3ce1812a8bbce0970735169d
Katarzyna Kropiak <k.kropiak@gmail.com>
2022-07-27 11:17:36 +0200

    31.2 Facebook RegistrationForm

--- kodilla-testing2/src/main/java/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/facebook/FacebookTestingApp.java
@@ -8,13 +8,12 @@ import org.openqa.selenium.support.ui.Select;
 
 public class FacebookTestingApp {
 
-    public static final String XPATH_COOKIES = "/html/body/div/div/div/div/div/div/div/button[1]";
-    public static final String XPATH_REGISTRATION = "/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[5]/a";
+    public static final String XPATH_COOKIES = "//div/button[contains(@class,\"_42ft 4jy0 9xo6 4jy3 4jy1 selected _51sy\")]";
+    public static final String XPATH_REGISTRATION = "//div/a[contains(@class,\"_42ft 4jy0 6lti 4jy6 4jy2 selected _51sy\")]";
     public static final String XPATH_REGISTRATION_WINDOW = "/html/body/div[3]/div[2]/div/div/div[2]/div/div/div[1]";
-    public static final String XPATH_SELECT_DAY = "/html/body/div[3]/div[2]/div/div/div[2]/div/div/div[1]/form/div[1]/div[5]/div[2]/span/span/select[1]";
-    public static final String XPATH_SELECT_MONTH = "/div[contains(@class, _5k_5)]/span/span/select[2]";
-    public static final String XPATH_SELECT_YEAR = "//*[@id=\"year\"]";
-//    public static final String XPATH_INPUT = "/html/body/div[3]/div[2]/div/div/div[2]/div/div/div[1]/form/div[1]/div[1]/div[1]/div[1]/div/input";
+    public static final String XPATH_SELECT_DAY = "//div[contains(@class, \"_58mq _5dbb\")]/div[2]/span/span/select[1]";
+    public static final String XPATH_SELECT_MONTH = "//div[contains(@class, \"_58mq _5dbb\")]/div[2]/span/span/select[2]";
+    public static final String XPATH_SELECT_YEAR = "//div[contains(@class, \"_58mq _5dbb\")]/div[2]/span/span/select[3]";
 
 
     public static void main(String[] args) throws InterruptedException {
@@ -28,20 +27,16 @@ public class FacebookTestingApp {
 
         while (!driver.findElement(By.xpath(XPATH_REGISTRATION_WINDOW)).isDisplayed());
 
-//        WebElement searchField = driver.findElement(By.xpath(XPATH_INPUT));
-//        searchField.wait(5000);
-//        searchField.sendKeys("James");
 
         Thread.sleep(7000);
 
         WebElement selectDayCombo = driver.findElement(By.xpath(XPATH_SELECT_DAY));
         Select selectDay = new Select(selectDayCombo);
-    //    selectDay.wait(5000);
-        selectDay.selectByValue("2");
+        selectDay.selectByIndex(0);
 
         WebElement selectMonthCombo = driver.findElement(By.xpath(XPATH_SELECT_MONTH));
         Select selectMonth = new Select(selectMonthCombo);
-        selectMonth.selectByIndex(2);
+        selectMonth.selectByValue("2");
 
         WebElement selectYearCombo = driver.findElement(By.xpath(XPATH_SELECT_YEAR));
         Select selectYear = new Select(selectYearCombo);
